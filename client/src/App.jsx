9a826d7f92b45b283ad77a4ff2adebb6ab3a1cd1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-07-03 16:36:34 +0530

    .

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Box from '@mui/material/Box';
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "@mui/styles";
+//import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    {/* <Box sx={{ backgroundColor: '#000' }}> */}
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    {/* </Box> */}
+  </BrowserRouter>
+);
+
+export default App;
