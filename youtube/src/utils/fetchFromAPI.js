2c5bbfa92aa68f8ccf464186bde181634b531d03
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-07-12 11:36:10 +0530

    fetching data using axios and rapid api

---
+++ youtube/src/utils/fetchFromAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: '50',
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+ 
+export const fetchFromAPI = async (url) => {
+   const {data} =await axios.get(`${BASE_URL}/${url}`, options);
+
+   return data;
+}
